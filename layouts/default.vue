4da8c7e35b101f8000c1a892cf10eb3932ddba89
Emmanuel Sitinjak <surabi.eman@gmail.com>
2021-07-18 01:27:53 +0900

    layout

---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <BaseNavbar />
+    <Nuxt />
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,22 @@
 <template>
-  <Tutorial/>
+  <div>
+    <FrontCarousel class="mb-5" />
+    <b-container>
+      <b-row>
+        <b-col cols="12"><h1>Recommendation</h1></b-col>
+        <b-col cols="12" lg="3" md="4" v-for="index in 10" :key="index">
+          <FrontProductItem  />
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  layout: 'default',
+  layout (context) {
+    return 'default';
+  }
+}
 </script>
