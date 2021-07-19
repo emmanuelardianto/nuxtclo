b0cff415be6b232951acb0e2ff31b203a20979fb
Emmanuel Sitinjak <surabi.eman@gmail.com>
2021-07-20 01:33:06 +0900

    index page

--- components/base/Navbar.vue
+++ components/base/Navbar.vue
@@ -1,39 +1,43 @@
 <template>
   <div>
-      <b-navbar toggleable="lg" type="light" variant="light">
+      <b-navbar toggleable="lg" type="light" class="bg-white">
       <b-container>
-        <b-navbar-brand href="#">SimpleCommerce</b-navbar-brand>
+        <b-navbar-brand href="#">
+          <img src="~/assets/header.svg" alt="logo" title="uniqlo">
+        </b-navbar-brand>
 
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
-            <b-nav-item href="#">Link</b-nav-item>
-            <b-nav-item href="#" disabled>Disabled</b-nav-item>
+            <b-nav-item href="#">Women</b-nav-item>
+            <b-nav-item href="#">Men</b-nav-item>
+            <b-nav-item href="#">Kids</b-nav-item>
+            <b-nav-item href="#">Baby</b-nav-item>
           </b-navbar-nav>
 
           <!-- Right aligned nav items -->
           <b-navbar-nav class="ml-auto">
             <b-nav-form>
-              <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-              <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+              <b-input-group>
+                  <b-form-input squared placeholder="Search"></b-form-input>
+                  <b-input-group-append>
+                    <b-button squared variant="outline-secondary"><i class="fa fa-search"></i></b-button>
+                  </b-input-group-append>
+              </b-input-group>
             </b-nav-form>
+            <b-nav-item href="#"><i class="fa fa-store"></i></b-nav-item>
+            <b-nav-item href="#"><i class="far fa-user"></i></b-nav-item>
+            <b-nav-item href="#"><i class="far fa-heart"></i></b-nav-item>
+            <b-nav-item href="#"><i class="fa fa-shopping-cart"></i></b-nav-item>
 
-            <b-nav-item-dropdown text="Lang" right>
-              <b-dropdown-item href="#">EN</b-dropdown-item>
-              <b-dropdown-item href="#">ES</b-dropdown-item>
-              <b-dropdown-item href="#">RU</b-dropdown-item>
-              <b-dropdown-item href="#">FA</b-dropdown-item>
-            </b-nav-item-dropdown>
-
-            <b-nav-item-dropdown right>
-              <!-- Using 'button-content' slot -->
+            <!-- <b-nav-item-dropdown right>
               <template #button-content>
                 <em>User</em>
               </template>
               <b-dropdown-item href="#">Profile</b-dropdown-item>
               <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-            </b-nav-item-dropdown>
+            </b-nav-item-dropdown> -->
           </b-navbar-nav>
         </b-collapse>
       </b-container>
@@ -47,6 +51,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.navbar-light .navbar-nav .nav-link {
+  color: #1b1b1b;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,78 @@
   <div>
     <FrontCarousel class="mb-5" />
     <b-container>
-      <b-row v-if="!isLoading">
-        <b-col cols="12"><h1>Recommendation</h1></b-col>
-        <b-col cols="12" lg="3" md="4" v-for="(item, index) in products.data" :key="index">
-          <FrontProductItem :product="item"  />
+      <!-- special links -->
+      <b-row class="mb-5">
+        <b-col v-for="link in specialLinks" :key="link" cols="6" lg="3" md="6" class="text-center my-4">
+          <h4>{{ link }}</h4>
+          <a href="#" v-for="gender in genders" :key="gender" class="mx-2 text-dark border-bottom text-uppercase pb-1">{{ gender }}</a>
         </b-col>
       </b-row>
-      <div v-else>Loading <i class="fa fa-spin fa-spinner"></i></div>
+      <!-- topics -->
+      <b-row class="my-3">
+        <b-col v-for="topic in topics" :key="topic.title" cols="12" md="4">
+          <b-row class="my-2">
+            <b-col cols="6"><img :src="require('~/assets/front/' + topic.image)" :alt="topic.title" :title="topic.title" class="w-100"></b-col>
+            <b-col cols="6">
+              <h5>{{ topic.title }}</h5>
+              <p>{{ topic.description }}</p>
+            </b-col>
+          </b-row>
+        </b-col>
+      </b-row>
+      <!-- promotion 01 -->
+      <h2>ふだん着の日が、人生になる。</h2>
+      <b-row class="my-3">
+        <b-col v-for="promotion in promotions" :key="promotion.title" cols="12" md="6" class="my-3">
+          <img src="~/assets/front/promotion-01.jpeg" :alt="promotion.title" :title="promotion.title" class="w-100 mb-3">
+          <h5>{{ promotion.title }}</h5>
+          <p>{{ promotion.description }}</p>
+        </b-col>
+      </b-row>
+      <b-row class="justify-content-center my-4">
+        <b-col cols="12" md="10" lg="8">
+          <b-button block variant="outline-dark" class="font-weight-bold" squared>特集ページを見る</b-button>
+        </b-col>
+      </b-row>
+      <h2>About LifeWear</h2>
+      <b-row class="my-3">
+        <b-col v-for="promotion in promotions" :key="promotion.title" cols="12" md="6" lg="3" class="my-3">
+          <img src="~/assets/front/promotion-01.jpeg" :alt="promotion.title" :title="promotion.title" class="w-100 mb-3">
+          <h5>{{ promotion.title }}</h5>
+          <p>{{ promotion.description }}</p>
+        </b-col>
+      </b-row>
+      <b-row class="justify-content-center my-4">
+        <b-col cols="12" md="10" lg="8">
+          <b-button block variant="outline-dark" class="font-weight-bold" squared>About LifeWearを見る</b-button>
+        </b-col>
+      </b-row>
+      <h2>インフォメーション</h2>
+      <b-row class="my-3">
+        <b-col v-for="info in informations" :key="info" cols="12" md="3" lg="4" class="my-2">
+          <a href="#" class="text-dark">{{ info }}</a>
+        </b-col>
+      </b-row>
     </b-container>
+    <div class="bg-light py-4">
+      <b-container>
+        <div class="mb-3"><a href="#" v-for="footer in footers" :key="footer" class="text-dark mr-3 border-right pr-3">{{ footer }}</a></div>
+        <div class="mb-5">
+          <span class="font-weight-bold mr-3">グループブランド　オンラインストア</span>
+          <a href="#" v-for="brand in footerGlobalBrands" :key="brand" class="text-dark mr-3 border-right pr-3">{{ brand }}</a>
+        </div>
+        <div class="border-bottom mb-3"></div>
+        <b-row>
+          <b-col cols="12" md="6" class="text-center text-lg-left text-md-left my-3" >Copyright © NUXTCLO Co., Ltd. All rights reserved.</b-col>
+          <b-col cols="12" md="6" class="text-center text-lg-right text-md-right my-3">
+            <i class="fab fa-2x fa-facebook mr-2"></i>
+            <i class="fab fa-2x fa-instagram mr-2"></i>
+            <i class="fab fa-2x fa-twitter mr-2"></i>
+            <i class="fab fa-2x fa-youtube"></i>
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
   </div>
 </template>
 
@@ -25,7 +89,81 @@ export default {
   data() {
     return {
       products: [],
-      isLoading: true
+      isLoading: true,
+      specialLinks: [
+        '新作商品',
+        '今週のチラシ',
+        '期間限定価格商品',
+        '値下げ商品'
+      ],
+      genders: [
+        'Women',
+        'Men',
+        'Kids',
+        'Baby'
+      ],
+      topics: [
+        {
+          image : "topic-01.jpeg",
+          title : "7月22日(木)20時　夏物セール　夏までに買い足したい、マストバイTOP3",
+          description: "ご不在時にも荷物を受取れる置き配サービスを指定できるようになりました。"
+        },
+        {
+          image : "topic-02.jpeg",
+          title : "NUXTCLO PAY",
+          description: "ご不在時にも荷物を受取れる置き配サービスを指定できるようになりました。"
+        },
+        {
+          image : "topic-03.png",
+          title : "置き配サービスがスタート",
+          description: "ご不在時にも荷物を受取れる置き配サービスを指定できるようになりました。"
+        },
+      ],
+      promotions: [
+        {
+          image : "topic-01.jpeg",
+          title : "7月22日(木)20時　夏物セール　夏までに買い足したい、マストバイTOP3",
+          description: "ご不在時にも荷物を受取れる置き配サービスを指定できるようになりました。"
+        },
+        {
+          image : "topic-02.jpeg",
+          title : "NUXTCLO PAY",
+          description: "ご不在時にも荷物を受取れる置き配サービスを指定できるようになりました。"
+        },
+        {
+          image : "topic-03.png",
+          title : "置き配サービスがスタート",
+          description: "ご不在時にも荷物を受取れる置き配サービスを指定できるようになりました。"
+        },
+        {
+          image : "topic-03.png",
+          title : "置き配サービスがスタート2",
+          description: "ご不在時にも荷物を受取れる置き配サービスを指定できるようになりました。"
+        },
+      ],
+      informations: [
+        "店舗における感染防止対策",
+        "着こなし発見アプリStyleHint",
+        "ユニクロアプリで買い物上手に",
+        "法人様・企業様へ大量購入/プリント加工サービス",
+        "プレスリリース",
+        "ショッピングバッグ有料化のお知らせ"
+      ],
+      footers: [
+        "会員情報",
+        "返金・交換について",
+        "お問い合わせ",
+        "利用規約",
+        "プライバシーポリシー",
+        "推奨環境",
+        "企業情報"
+      ],
+      footerGlobalBrands: [
+        "ジーユー",
+        "セオリー",
+        "プラステ",
+        "コントワー・デ・コトニエ"
+      ]
     }
   },
   async fetch() {
